397bce5bdce0985cc168468ef1e16836a17f394b
rajat10121993 <acharya.raj@husky.neu.edu>
2018-11-27 19:22:20 -0500

    Adding Cars Component

--- car-rental-application/src/app/app.module.ts
+++ car-rental-application/src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { CarsComponent } from './cars/cars.component';
+import { CarsService } from './services/cars.services';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFontAwesomeModule
   ],
-  providers: [],
+  providers: [CarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
